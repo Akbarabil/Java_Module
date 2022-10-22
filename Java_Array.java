c4924ebab51b3adf364cc3aaa6dc5764a11502e3
boltzy <96470039+Akbarabil@users.noreply.github.com>
2022-10-22 18:11:43 +0700

    Update Java_Array.java

--- Java_Array.java
+++ Java_Array.java
@@ -3,5 +3,11 @@ public class Main {
     String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
     cars[0] = "Opel";
     System.out.println(cars[0]);
+    //multidimensi
+    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+     for (int i = 0; i < myNumbers.length; ++i) {
+        for(int j = 0; j < myNumbers[i].length; ++j) {
+           System.out.println(myNumbers[i][j]);
+        }
   }
 }
